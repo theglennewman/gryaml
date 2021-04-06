6ec68514ade20d5b38e3361b552b228e1b986fff
theglennewman <15897605+theglennewman@users.noreply.github.com>
2021-04-05 22:41:55 -0400

    print contents of a file works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+
+/* TODO
+
+to start?
+- let's loop over a file, print every line
+- then we'll start identifying line types
+
+place to document the yaml rules?
+- stuff like... spaces are allowed, tabs are not
+*/
+
+fn identify_line_type() -> &'static str {
+    /*
+    lines can have...
+    key/values
+    multiple docs can be in a yaml file, separated by: ---
+    end of doc can be marked by: ...
+    or the line can be comments
+    */
+
+    "it's a line"
+}
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+fn file_to_string() -> String {
+    // we could probably get the $(pwd) out of std::env::current_dir()
+    // but for now let's just hard code it...
+    
+    //lol I don't even understand string concatenation
+    //let yaml_dir = "/mnt/vmshares/q_win10_share/gryaml/yaml_files"
+
+    //just hardcode the whole dumb file path
+    let file_path = 
+      Path::new("/mnt/vmshares/q_win10_share/gryaml/yaml_files/example.yaml");
+    let display = file_path.display();
+
+    println!("try to open: {}", display);
+    let mut file = match File::open(&file_path) {
+        Err(why) => panic!("could not open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("could not read {}: {}", display, why),
+        Ok(_) => print!("{} contents...\n{}", display, s),
+    }
+
+    s
+}
+
 fn main() {
-    println!("gryaml");
+    // settings for paths? yaml files to play with...
+
+    // we need file i/o
+
+    // open a yaml file
+
+    // parse a yaml file -> loop over lines? id what "kind" of line it is
+
+    // do stuff depending on what kind of line it is
+
+    let stringy_file = file_to_string();
+    println!("stringified file... {}", stringy_file);
 }
