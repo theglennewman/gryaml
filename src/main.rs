3368b19dace8f89d03e59b93e7be239ba56ecb0f
theglennewman <15897605+theglennewman@users.noreply.github.com>
2021-04-07 21:08:01 -0400

    broken: need to figure out results

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,18 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
 fn file_to_string() -> String {
+    let correct_filename =
+        "/mnt/vmshares/q_win10_share/gryaml/yaml_files/example.yaml";
+    println!("file path should be:\n\t{}", correct_filename);
+
     // we could probably get the $(pwd) out of std::env::current_dir()
     // but for now let's just hard code it...
+
+    //hmmm.... current_dir returns a "std::result::Result<PathBuf, std::io::Error"
+    // which does not have a display field... So I'm supposed to be doing something with this
+    // more properly...
+    let cur_dir = std::env::current_dir().display;
+    println!("current_dir is: {}", cur_dir.display());
     
     //lol I don't even understand string concatenation
     //let yaml_dir = "/mnt/vmshares/q_win10_share/gryaml/yaml_files"
@@ -31,11 +41,15 @@ fn file_to_string() -> String {
         Ok(file) => file,
     };
 
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-        Err(why) => panic!("could not read {}: {}", display, why),
-        Ok(_) => print!("{} contents...\n{}", display, s),
-    }
+    // read file into s
+    // let mut s = String::new();
+    // match file.read_to_string(&mut s) {
+    //     Err(why) => panic!("could not read {}: {}", display, why),
+    //     Ok(_) => print!("{} contents...\n{}", display, s),
+    // }
+
+    // or just hard code it...
+    let s = String::from("<totally file contents>");
 
     s
 }
@@ -51,6 +65,6 @@ fn main() {
 
     // do stuff depending on what kind of line it is
 
-    //let stringy_file = file_to_string();
+    println!("{}", file_to_string());
     //println!("stringified file... {}", stringy_file);
 }
